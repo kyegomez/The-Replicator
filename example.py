79003b3d42ce83b73101ca795d6cbd1c683a96c8
Kye <kye@apacmediasolutions.com>
2023-09-23 16:51:47 -0400

    clean upg

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ from replicate.prompts import system_content3, user_input3
 from replicate.main import Replicator
 
 
-user = "Create a time series model for predicting the stock market"
+user = "Create a time series model for predicting the stock market, use the code interpreter tool"
 
 replicator = Replicator(
     system=user_input3,
